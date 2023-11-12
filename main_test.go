06c3404e6e69a383825927ba3d46bb7f68a0350d
awcodify <awcodify@gmail.com>
2023-11-12 15:50:51 +0700

    add most productive time and day feature

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/google/go-github/v38/github"
 )
@@ -12,6 +13,7 @@ type MockGitHubAPI struct {
 	UserResponse         *github.User
 	ListRepositoriesResp []*github.Repository
 	ListLanguagesResp    map[string]int
+	ListAllCommitsResp   []*github.RepositoryCommit
 }
 
 func (m *MockGitHubAPI) GetUser(ctx context.Context, user string) (*github.User, *github.Response, error) {
@@ -24,6 +26,10 @@ func (m *MockGitHubAPI) ListRepositories(ctx context.Context, user string, opts 
 
 func (m *MockGitHubAPI) ListLanguages(ctx context.Context, user, repo string) (map[string]int, *github.Response, error) {
 	return m.ListLanguagesResp, nil, nil
+}
+
+func (m *MockGitHubAPI) ListAllCommits(ctx context.Context, user string, opts *github.CommitsListOptions) ([]*github.RepositoryCommit, *github.Response, error) {
+	return m.ListAllCommitsResp, nil, nil
 }
 
 func TestSummarizeGitHubProfile(t *testing.T) {
@@ -105,5 +111,92 @@ func TestCombineLanguages(t *testing.T) {
 		if lang != expectedCombined[i] {
 			t.Errorf("Combined languages are incorrect, got: %s, want: %s", lang, expectedCombined[i])
 		}
+	}
+}
+
+func TestCalculateMostProductiveDay(t *testing.T) {
+	// Mock data for testing
+	mockUser := &github.User{
+		Login: github.String("testuser"),
+		Name:  github.String("Test User"),
+	}
+
+	// Create time objects with the desired dates
+	sunday := time.Date(2023, time.November, 12, 0, 0, 0, 0, time.UTC) // 12 Nov 2023 => Sunday
+	monday := time.Date(2023, time.November, 13, 0, 0, 0, 0, time.UTC) // 13 Nov 2023 => Monday
+
+	// Create mock commits with the specified dates
+	// 4 m
+	mockCommits := []*github.RepositoryCommit{
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &sunday}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &sunday}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &sunday}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &sunday}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &monday}}},
+	}
+
+	// Create an instance of the MockGitHubAPI
+	mockGitHubAPI := &MockGitHubAPI{
+		UserResponse:         mockUser,
+		ListRepositoriesResp: nil,         // Repositories list not needed for this test
+		ListLanguagesResp:    nil,         // Languages list not needed for this test
+		ListAllCommitsResp:   mockCommits, // Set the mock commits for ListAllCommits
+	}
+
+	// Call the function to be tested
+	mostProductiveDay, err := calculateMostProductiveDay(context.Background(), mockGitHubAPI)
+	if err != nil {
+		t.Errorf("Error in calculateMostProductiveDay: %v", err)
+	}
+
+	// Add assertions to verify the expected output
+	expectedDay := "Sunday" // Assuming the first commit is on a Monday
+	if mostProductiveDay != expectedDay {
+		t.Errorf("Most productive day is incorrect, got: %s, want: %s", mostProductiveDay, expectedDay)
+	}
+}
+
+func TestCalculateMostProductiveTime(t *testing.T) {
+	// Mock data for testing
+	mockUser := &github.User{
+		Login: github.String("testuser"),
+		Name:  github.String("Test User"),
+	}
+
+	// Create time objects with the desired dates
+	time1 := time.Date(2023, time.November, 12, 10, 0, 0, 0, time.UTC)
+	time2 := time.Date(2023, time.November, 13, 0, 0, 0, 0, time.UTC)
+	time3 := time.Date(2023, time.November, 14, 10, 0, 0, 0, time.UTC)
+	time4 := time.Date(2023, time.November, 15, 10, 0, 0, 0, time.UTC)
+	time5 := time.Date(2023, time.November, 16, 10, 0, 0, 0, time.UTC)
+
+	// Create mock commits with the specified dates
+	// 4 m
+	mockCommits := []*github.RepositoryCommit{
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &time1}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &time2}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &time3}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &time4}}},
+		{Commit: &github.Commit{Author: &github.CommitAuthor{Date: &time5}}},
+	}
+
+	// Create an instance of the MockGitHubAPI
+	mockGitHubAPI := &MockGitHubAPI{
+		UserResponse:         mockUser,
+		ListRepositoriesResp: nil, // Repositories list not needed for this test
+		ListLanguagesResp:    nil, // Languages list not needed for this test
+		ListAllCommitsResp:   mockCommits,
+	}
+
+	// Call the function to be tested
+	mostProductiveTime, err := calculateMostProductiveTime(context.Background(), mockGitHubAPI)
+	if err != nil {
+		t.Errorf("Error in calculateMostProductiveTime: %v", err)
+	}
+
+	// Add assertions to verify the expected output
+	expectedTime := "10:00 - 11:00" // Assuming the first commit is at 10:00
+	if mostProductiveTime != expectedTime {
+		t.Errorf("Most productive time is incorrect, got: %s, want: %s", mostProductiveTime, expectedTime)
 	}
 }
